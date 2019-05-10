82e31fdcddde26ac2495f144ea441c91dbe07062
Sungmiii <christyksmile@gmail.com>
2019-05-10 16:47:33 +1200

    basic classbased toggle

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = { resource: 'posts' }
     render() {
         return (
-            <div>app</div>
+            <div className="ui container">
+                <div className="content">
+                    <button
+                        className="ui button primary"
+                        onClick={() => this.setState({ resource: 'posts' })}
+                    >
+                        POSTs
+                    </button>
+                    <button
+                        onClick={() => this.setState({ resource: 'todos' })}
+                        className="ui button"
+                    >
+                        TODOs
+                    </button>
+                </div>
+                {this.state.resource}
+            </div>
         )
     }
 };
