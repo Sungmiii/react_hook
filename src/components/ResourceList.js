6af70759f8438b23d8d29115d990f9a0dfd8b21d
Sungmiii <christyksmile@gmail.com>
2019-05-15 12:54:29 +1200

    axios to fectch json placeholder

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
+import axios from 'axios';
+
 class ResourceList extends React.Component {
+
+    state = { resources: [] };
+
+    async componentDidMount() {
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+
+        this.setState({ resources: res.data })
+
+    }
+
+    async componentDidUpdate(prevProps) {
+        if (prevProps.resource !== this.props.resource) {
+            const res = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+
+            this.setState({ resources: res.data })
+        }
+    }
+
+
     render() {
         return (
             <div>
-                <h3>{this.props.resource}</h3>
+                <h3>{this.state.resources.length}</h3>
             </div>
         )
     }
-}
+};
 
 export default ResourceList;
